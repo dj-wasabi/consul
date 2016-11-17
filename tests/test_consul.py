939fbb7dc9273fe7a2f8d4cf5317aec35ec58c4b
Werner Dijkerman <werner@dj-wasabi.nl>
2016-11-17 19:56:24 +0100

    Fixing Jenkins stages; Corrected user/group for config file

--- tests/test_consul.py
+++ tests/test_consul.py
@@ -12,7 +12,7 @@ def test_socker(Socket):
 
 def test_config_file(File):
     config_file = File("/consul/config/config.json")
-    assert config_file.user == "root"
+    assert config_file.user == "consul"
     assert config_file.group == "root"
     assert config_file.mode == 0o644
     assert config_file.contains('"data_dir": "/consul/data"')
